3b6416210a6cac1e8998b58bf198d4ecf63a16a8
CapSap <cshotam@gmail.com>
2024-08-20 21:05:44 +1000

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel("./data/20240722 Pronto Master.xlsx")
+# print(df)
+
+
+# ice = pd.read_excel("./data/ICE Wi24 Indent Internal w Targets 26.06.23.xlsx")
+df_ice = pd.read_excel("data/ice.xlsx")
+print(df_ice)
